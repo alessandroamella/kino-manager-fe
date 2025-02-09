b7a9d03ec43cdae04efb20d924af77b90787d2cd
Alessandro Amella <info@bitrey.it>
2025-02-09 16:08:36 +0100

    rm /purchase vite proxt

--- vite.config.ts
+++ vite.config.ts
@@ -21,11 +21,6 @@ export default defineConfig({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
-      '/purchase': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        ws: true,
-      },
     },
   },
 });
